4007e3f1e6539f22fd65bb432be13a8bfe46c108
lvdacong <lvdacong314@gmail.com>
2026-01-16 16:43:30 +0800

    1

--- gcp.bat
+++ gcp.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Running git add, commit, push...
+echo Running git add, commit, push..........................
 git add .
 git commit -m "1"
 git push
-echo Done!
+echo Done!!!!!!!!!!!!!!
